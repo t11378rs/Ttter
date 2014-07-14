2528a7ad62c897a541f58ba92c81860ac1b91e04
t11378rs <ryo0717@gmail.com>
2014-07-15 01:56:38 +0900

    done. this is enough for beta.

--- Ttter3/Ttter3.ino
+++ Ttter3/Ttter3.ino
@@ -12,7 +12,7 @@ const byte TEST = 0;
 const byte PRODUCTION = 1;
 //const int LED_PIN = 13;
 const int BUTTON_PIN = 7;
-const int SPEAKER_PIN = 9;
+const int SPEAKER_PIN = 8;
 const char TOKEN[] = "257882187-WBB2XIkhdbzicQIrl9G9X3dkcOsUlhTXx7oRayZY";
 //byte MAC[] = { 0x00, 0x16, 0x3E, 0x5C, 0xF2, 0x7E };
 //byte IP[] = { 192, 168, 2, 104 };
@@ -110,7 +110,7 @@ byte morse_msg_cursor = 0;
 byte msg_cursor = 0;
 
 // variable to demonstrate development_phase
-const byte dev_phase = TEST;
+const byte dev_phase = PRODUCTION;
 
 void setup()
 {
@@ -172,25 +172,29 @@ void loop()
   unsigned long pushed_interval = 0;//ボタンが押されている時間を入れる
   unsigned long released_interval = 0;//ボタンが離されている時間を入れる
 
-  unsigned long just_before_pushed = millis();//ボタンが押される直前の時間
+  //もし押されていたら音を出す
   if(digitalRead(BUTTON_PIN) == HIGH){
     tone(SPEAKER_PIN, 262);
+  //}else{
+  //  noTone(SPEAKER_PIN);
   }
+
+  unsigned long just_before_pushed = millis();//ボタンが押される直前の時間
   while(digitalRead(BUTTON_PIN) == HIGH){//ボタンが押されている間
-    pushed_interval = millis() - just_before_pushed; //ボタンが押されている秒数を調べる
+    //do nothing.
   }
-  print_s_ul_s("pushed_interval is ", pushed_interval, " sec");
+  unsigned long just_after_pushed = millis();
+  pushed_interval = just_after_pushed - just_before_pushed;
+  print_s_ul_s("pushed_interval is ", pushed_interval, " msec");
   if(digitalRead(BUTTON_PIN) == LOW){//LOWになった瞬間
-    if(SINGLE_TIME*3 < pushed_interval){    //長音分(single*3)押されていたら3~
+    if(SINGLE_TIME*2.5 < pushed_interval){    //長音分(single*3)押されていたら3~
       morse_msg.setCharAt(morse_msg_cursor, '-');//長音をmorse_msgに入れる
       Serial.print("add ");
-      //Serial.println(morse_msg.charAt(morse_msg_cursor));
       Serial.println(morse_msg);
       morse_msg_cursor++;
     }else if(SINGLE_TIME < pushed_interval){//単音分(single)押されていたら 1~2
       morse_msg.setCharAt(morse_msg_cursor, '*');//単音をmorse_msgに入れる
       Serial.print("add ");
-      //Serial.println(morse_msg.charAt(morse_msg_cursor));
       Serial.println(morse_msg);
       morse_msg_cursor++;
     }else{//単音以下の時は
@@ -202,21 +206,22 @@ void loop()
 
   unsigned long just_before_released = millis();
   while(digitalRead(BUTTON_PIN) == LOW){//ボタンが離されている間
-    released_interval = millis() - just_before_released; //ボタンが押されていない秒数を調べる
+    //do nothing
   }
-  print_s_ul_s("released_interval is ", released_interval, " sec");
+  unsigned long just_after_released = millis();
+  released_interval = just_after_released - just_before_released;
+  print_s_ul_s("released_interval is ", released_interval, " msec");
   if(digitalRead(BUTTON_PIN) == HIGH){//ボタンが押された瞬間
     if(SINGLE_TIME*14 < released_interval){//14拍以上離されていたら 14~
-      //何もしない
+      //do nothing
       Serial.println("Unknown: too long released interval!");
-    }else if(SINGLE_TIME*10 < released_interval){//7拍分離されていたら 7~13
+    //}else if(SINGLE_TIME*10 < released_interval){//7拍分離されていたら 7~13
       //morse_msg[morse_msg_cursor] = ' ';//空白をmorse_msgに入れる
       //Serial.println("add space");
       //morse_msg_cursor++;
-      //何もしない
-    }else if(SINGLE_TIME*3 < released_interval){//3拍分離されていたら 3~7
+    }else if(SINGLE_TIME*4 < released_interval){//3拍分離されていたら 3~7
       char c = translation(morse_msg); //morse_msgを翻訳
-      if(c=='x'){//翻訳したものが終了信号だったら、msgを投稿
+      if(c == 'x'){//翻訳したものが終了信号だったら、msgを投稿
         post(msg);
         reset_all();
       }else if(c=='o'){//翻訳したものが開始信号だったら、全てをリセット
@@ -227,11 +232,13 @@ void loop()
         morse_msg="EEEEEEEEEE";//morse_msgを空にする
         morse_msg_cursor=0;//morse_msg_cursorも空にする
       }
-    }else if(SINGLE_TIME < released_interval){//1拍分離されていたら
+    }else{//1拍分離されていたら
       //何もしない
       Serial.println("Unknown: too short released interval!");
     }
   }
+
+  delay(50);
 }
 
 void reset_all(){
@@ -262,26 +269,6 @@ char translation(String morse){
   return '#';
 }
 
-/*
-int myCompareString(char text1[], char text2[]){
-  //サイズを調べる
-  if( sizeof(text1) != sizeof(text2)){
-    return -1;
-  }else{
-    for(int i=0; i<=sizeof(text1); i++){
-      if(text1[i] != text2[i]){
-        return -1;
-      }else{
-        //do nothing
-      }
-      delay(100);
-    }
-    return 1;
-  }
-}
-*/
-
-
 void print_s_ul_s(char text1[], unsigned long digit, char text2[]){
   Serial.print(text1);
   Serial.print(digit);
@@ -295,18 +282,18 @@ void post(String str){
     post_str[i] = str.charAt(i);
   }
 
-  //Serial.println("connecting ...");
+  Serial.println("connecting ...");
   if(dev_phase == PRODUCTION){
     if (twitter.post(post_str)) {
       int status = twitter.wait(&Serial);
       if (status == 200) {
-        //Serial.println("OK.");
+        Serial.println("OK.");
       } else {
-        //Serial.print("failed : code ");
+        Serial.print("failed : code ");
         Serial.println(status);
       }
     } else {
-      //Serial.println("connection failed.");
+      Serial.println("connection failed.");
     }
   }else if(dev_phase == TEST){
     //Serial.print("post ");
